996ba29728cf1cfb9cc19dc77c63d1247847d73a
Denys Sheremeta <DSheremeta@franpos.com>
2025-09-06 19:50:26 +0300

    solution

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+echo "Creating Kubernetes cluster using kind"
+kind create cluster --config cluster.yml
+
+echo "Creating namespace"
+kubectl apply -f ./.infrastructure/namespace.yml
+
+echo "Creating other manifests"
+kubectl apply -f ./.infrastructure/
+
+echo "Done!"
